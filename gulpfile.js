ff609ab360bac3ea4de2e6275e237c98591c04ff
Samanta <slaragiannone@gmail.com>
2018-10-18 20:22:43 +0100

    prueba con babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,23 @@
-let gulp = require('gulp');
-let less = require('gulp-less');
-let postcss = require('gulp-postcss');
-let cssnext = require('postcss-cssnext');
-let cleancss = require('gulp-clean-css');
-let rename = require('gulp-rename');
+let rollup = require('rollup');
+let rollupBabel = require('rollup-plugin-babel');
+let rollupUglify = require('rollup-plugin-uglify');
 
-let postcssOptions = [cssnext];
-
-gulp.task('css', () => {
-    return gulp.src('src/css/*.less')
-        .pipe(less())
-        .pipe(postcss(postcssOptions))
-        .pipe(postcss([require('autoprefixer')]))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(cleancss())
-        .pipe(rename({ suffix: '.min'}))
-        .pipe(gulp.dest('dist/css'))
+gulp.task('js', () => {
+    return rollup.rollup({
+        input: './src/js/index.es6',
+        plugins: [
+            rollupBabel(),
+            rollupUglify(),
+        ]
+    }).then(bundle => {
+        return bundle.write({
+            file: './dist/js/index.min.js',
+            format: 'umd',
+            name: 'main'
+        });
+    });
 });
+
 
 // Primera prueba
 // let gulp = require('gulp');
@@ -39,5 +40,26 @@ gulp.task('css', () => {
 //             extra: 'src/css/includes/**/*.less',
 //         }))
 //         .pipe(less())
+//         .pipe(gulp.dest('dist/css'))
+// });
+
+// Tercera prueba
+// let gulp = require('gulp');
+// let less = require('gulp-less');
+// let postcss = require('gulp-postcss');
+// let cssnext = require('postcss-cssnext');
+// let cleancss = require('gulp-clean-css');
+// let rename = require('gulp-rename');
+
+// let postcssOptions = [cssnext];
+
+// gulp.task('css', () => {
+//     return gulp.src('src/css/*.less')
+//         .pipe(less())
+//         .pipe(postcss(postcssOptions))
+//         .pipe(postcss([require('autoprefixer')]))
+//         .pipe(gulp.dest('dist/css'))
+//         .pipe(cleancss())
+//         .pipe(rename({ suffix: '.min'}))
 //         .pipe(gulp.dest('dist/css'))
 // });
